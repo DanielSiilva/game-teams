36603c90f2ffe9bfab4e60da1fb1c57fdee15df4
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2023-06-08 20:57:58 -0300

    Feat: Utilizando Styled Components

--- gameteams/src/screens/Groups/index.tsx
+++ gameteams/src/screens/Groups/index.tsx
@@ -1,21 +1,13 @@
-import { Text, View, StyleSheet } from "react-native";
+import {
+  Container,
+  Title
+} from './styles'
 
 
 export  function Groups() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Groups</Text>
-    </View>
+    <Container>
+      <Title>Groups</Title>
+    </Container>
   )
 }
-
-const styles = StyleSheet.create({
-  container:{
-    flex:1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  text: {
-    color: '#000',
-  }
-})
