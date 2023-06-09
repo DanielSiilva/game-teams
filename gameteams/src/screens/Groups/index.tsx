5a4d798c0f0f943899fd55ba2314d97c27ca5212
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2023-06-09 13:03:42 -0300

    Feat: Componente List Empty

--- gameteams/src/screens/Groups/index.tsx
+++ gameteams/src/screens/Groups/index.tsx
@@ -9,11 +9,12 @@ import {
 import { Header } from '@components/Header'
 import { GroupCard } from '@components/GroupCard'
 import { FlatList } from 'react-native'
+import { ListEmpty } from '@components/ListEmpty'
 
 
 
 export  function Groups() {
-  const [groups, setGroups] = useState<string[]>(['Galera do Native'])
+  const [groups, setGroups] = useState<string[]>([])
 
   return (
     <Container>
@@ -29,6 +30,10 @@ export  function Groups() {
           <GroupCard
             title={item}
           />
+        )}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Que tal cadastrar a primeira turma?" />
         )}
       />
       
