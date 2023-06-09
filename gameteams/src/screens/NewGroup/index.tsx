01fc2978a4ec18bf9e0a0c83147f5239ebb2d539
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2023-06-09 17:18:06 -0300

    Feat: Criando Componente Input

--- gameteams/src/screens/NewGroup/index.tsx
+++ gameteams/src/screens/NewGroup/index.tsx
@@ -5,8 +5,10 @@ import {
   Content,
   Icon
 } from "./styles";
+
 import { Highlight } from "@components/Highlight";
 import { Button } from "@components/Button";
+import { Input } from "@components/Input";
 
 
 export function NewGroup(){
@@ -21,8 +23,11 @@ export function NewGroup(){
           title="Nova turma"
           subtitle="crie a turma para adicionar as pessoas"
         />
+        <Input />
+
         <Button 
           title="Criar"
+          style={{ marginTop: 20 }}
         />
       </Content>
     </Container>
