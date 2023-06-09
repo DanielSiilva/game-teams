d1fa3618ecb52f75112706fa5959fbe6dc4bad87
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2023-06-09 16:29:08 -0300

    Styles: centralizando o button

--- gameteams/src/screens/Groups/index.tsx
+++ gameteams/src/screens/Groups/index.tsx
@@ -1,34 +1,28 @@
-import { useState } from 'react'
-import { FlatList } from 'react-native'
+import { useState } from 'react';
+import { FlatList } from 'react-native';
+import { GroupCard } from '@components/GroupCard';
+import { Header } from '@components/Header';
+import { Highlight } from '@components/Highlight';
+import { ListEmpty } from '@components/ListEmpty';
+import { Button } from '@components/Button';
 
-import {
-  Container,
-} from './styles'
+import { Container } from './styles';
 
-import { Header } from '@components/Header'
-import { GroupCard } from '@components/GroupCard'
-import { ListEmpty } from '@components/ListEmpty'
-import { Button } from '@components/Button'
-import { Highlight } from '@components/Highlight'
-
-
-
-export  function Groups() {
-  const [groups, setGroups] = useState<string[]>([])
-
+export function Groups() {
+  const [groups, setGroups] = useState<string[]>([]);
   return (
     <Container>
-      <Header/>
+      <Header />
       <Highlight 
-        title='Turmas'
-        subtitle='jogue com a sua turma'
+        title="Turmas"
+        subtitle="jogue com sua turma"
       />
-      <FlatList
+      <FlatList 
         data={groups}
         keyExtractor={item => item}
-        renderItem={({item}) => (
-          <GroupCard
-            title={item}
+        renderItem={({ item }) => (
+          <GroupCard 
+            title={item} 
           />
         )}
         contentContainerStyle={groups.length === 0 && { flex: 1 }}
@@ -38,9 +32,8 @@ export  function Groups() {
       />
 
       <Button 
-        title='Criar nova Turma'
+        title='Criar nova turma'
       />
-      
     </Container>
-  )
+  );
 }
