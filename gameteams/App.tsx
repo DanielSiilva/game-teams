17749b321d7b220c6be4628059f18b30d7625a67
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2023-06-08 22:07:11 -0300

    Feat: Estilizando a StatusBar

--- gameteams/App.tsx
+++ gameteams/App.tsx
@@ -1,3 +1,4 @@
+import {StatusBar} from 'react-native'
 import {ThemeProvider} from 'styled-components/native'
 import theme from './src/theme/index';
 import {useFonts, Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/roboto'
@@ -12,6 +13,11 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar 
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
        {fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
    
