19673cfbb755162be038ec36573cb8e51f6dee42
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2023-06-09 22:27:35 -0300

    Feat: Criando a Interface Players

--- gameteams/App.tsx
+++ gameteams/App.tsx
@@ -3,7 +3,7 @@ import {ThemeProvider} from 'styled-components/native'
 import theme from './src/theme/index';
 import {useFonts, Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/roboto'
 
-import {Groups} from '@screens/Groups'
+import {Players} from '@screens/Players'
 import {Loading} from '@components/Loading'
 
 
@@ -19,7 +19,7 @@ export default function App() {
           backgroundColor="transparent"
           translucent
         />
-        {fontsLoaded ? <Groups /> : <Loading />}
+        {fontsLoaded ? <Players /> : <Loading />}
       </>
     </ThemeProvider>
    
