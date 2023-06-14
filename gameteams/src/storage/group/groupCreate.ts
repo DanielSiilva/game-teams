210466b97a087606ba5d703080412675928217ae
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2023-06-14 20:18:08 -0300

    Feat: Obtendo os grupos

--- gameteams/src/storage/group/groupCreate.ts
+++ gameteams/src/storage/group/groupCreate.ts
@@ -1,10 +1,12 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import { GROUP_COLLECTION } from '@storage/storageConfig';
+import { groupsGetAll } from './groupGetAll';
 
 export async function groupCreate(newGroup: string) {
   try {
-                              // Chave, valor
+    const storedGroups = await groupsGetAll();
+                              // Chave, valor => Precisa ser uma string
     await AsyncStorage.setItem(GROUP_COLLECTION, newGroup);
   } catch (error) {
     throw error;
