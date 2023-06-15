e73cb2ee258435e0a8b8ea8b3f64d943bb1544cd
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2023-06-14 22:02:22 -0300

    Feat: Abrindo turmas

--- gameteams/src/screens/Groups/index.tsx
+++ gameteams/src/screens/Groups/index.tsx
@@ -30,6 +30,10 @@ export function Groups() {
     }
   }
 
+  function handleOpenGroups(group: string){
+    navigation.navigate('players', {group})
+  }
+
   useFocusEffect(useCallback(() => {
     fetchGroups()
   },[]))
@@ -47,7 +51,8 @@ export function Groups() {
         keyExtractor={item => item}
         renderItem={({ item }) => (
           <GroupCard 
-            title={item} 
+            title={item}
+            onPress={() => handleOpenGroups(item)}
           />
         )}
         contentContainerStyle={groups.length === 0 && { flex: 1 }}
